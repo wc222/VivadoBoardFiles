559fb49a9716ba5862dd54cef2453aab577ab915
wc222 <2544199309@qq.com>
2024-01-22 16:17:56 +0800

    dest path

--- installBoardFile.bat
+++ installBoardFile.bat
@@ -5,10 +5,16 @@ set https_proxy=proxy.cse.cuhk.edu.hk:8000
 SET DOWNLOAD_URL=https://github.com/Digilent/vivado-boards/archive/master.zip
 SET DOWNLOAD_PATH=C:\temp
 SET UNZIP_PATH=C:\temp\
+SET DEST_PATH=C:\Xilinx\Vivado\2023.2\data\boards\board_files
  
 REM 创建下载文件夹
 IF NOT EXIST %DOWNLOAD_PATH% (
     mkdir %DOWNLOAD_PATH%
+)
+
+REM 创建目标文件夹
+IF NOT EXIST %DEST_PATH% (
+    mkdir %DEST_PATH%
 )
  
 REM 下载文件
@@ -18,7 +24,7 @@ REM 解压缩文件
 IF EXIST %DOWNLOAD_PATH%\master.zip (
     PowerShell -Command "Expand-Archive -Path '%DOWNLOAD_PATH%\master.zip' -DestinationPath '%UNZIP_PATH%'"
 )
- 
+
 REM 复制文件夹
 xcopy /E /Y %UNZIP_PATH%\vivado-boards-master\new\board_files C:\Xilinx\Vivado\2023.2\data\boards
  
