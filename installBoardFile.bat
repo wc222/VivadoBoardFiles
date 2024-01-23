21e5c9ae7a61167ab03f00424dc08d5db096be40
wc222 <2544199309@qq.com>
2024-01-23 14:16:42 +0800

    ppt

--- installBoardFile.bat
+++ installBoardFile.bat
@@ -1,34 +1,30 @@
-REM 设置下载文件路径和解压缩目录
-
-set https_proxy=proxy.cse.cuhk.edu.hk:8000
-
+REM set variables
+SET https_proxy=proxy.cse.cuhk.edu.hk:8000
 SET DOWNLOAD_URL=https://github.com/Digilent/vivado-boards/archive/master.zip
 SET DOWNLOAD_PATH=C:\temp
 SET UNZIP_PATH=C:\temp\
 SET DEST_PATH=C:\Xilinx\Vivado\2023.2\data\boards\board_files
  
-REM 创建下载文件夹
+REM create folders
 IF NOT EXIST %DOWNLOAD_PATH% (
     mkdir %DOWNLOAD_PATH%
 )
-
-REM 创建目标文件夹
 IF NOT EXIST %DEST_PATH% (
     mkdir %DEST_PATH%
 )
  
-REM 下载文件
+REM download board files
 curl -Lk -o %DOWNLOAD_PATH%\master.zip %DOWNLOAD_URL%
  
-REM 解压缩文件
+REM unzip board files
 IF EXIST %DOWNLOAD_PATH%\master.zip (
     PowerShell -Command "Expand-Archive -Path '%DOWNLOAD_PATH%\master.zip' -DestinationPath '%UNZIP_PATH%'"
 )
 
-REM 复制文件夹
+REM copy board files into vivado
 xcopy /E /Y %UNZIP_PATH%\vivado-boards-master\new\board_files %DEST_PATH%
  
-REM 删除临时文件夹
+REM remove temp files
 IF EXIST %UNZIP_PATH% (
     rmdir /S /Q %UNZIP_PATH%
 )
